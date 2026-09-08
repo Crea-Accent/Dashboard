0db4979861cc5c56d8dfb9225e8cde087acd273d
TheDummi <raydecrom@hotmail.com>
2026-09-08 16:38:16 +0200

    rm fix scripts

--- fix_comma.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('src/app/dashboard/projects/[id]/page.tsx', 'utf8');
-content = content.replace('Plus,,', 'Plus,');
-fs.writeFileSync('src/app/dashboard/projects/[id]/page.tsx', content, 'utf8');
--- fix_metadata_overflow.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('src/components/projects/Metadata.tsx', 'utf8');
-
-content = content.replace(/className="pt-4 overflow-hidden"/g, 'className="pt-4"');
-
-fs.writeFileSync('src/components/projects/Metadata.tsx', content, 'utf8');
